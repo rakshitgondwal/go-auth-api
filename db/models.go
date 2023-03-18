c3695b515c6c4d428a47502a69c5f94e50c692a6
Rakshit Gondwal <rakshitgondwal3@gmail.com>
2023-03-18 19:31:36 +0530

    add db schema

---
+++ db/models.go
@@ -0,0 +1,38 @@
+package db
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	ID           primitive.ObjectID `json:"_id" bson:"_id"`
+	Username     string             `json:"username" bson:"username"`
+	Email        string             `json:"email" bson:"email"`
+	Password     string             `json:"password" bson:"password"`
+	IsAdmin      bool               `json:"isAdmin" bson:"isAdmin"`
+	RefreshToken string             `json:"refreshToken" bson:"refreshToken"`
+	Organization string             `json:"organization" bson:"organization"`
+}
+
+type Tokens struct {
+	ID        primitive.ObjectID `json:"_id" bson:"_id"`
+	Token     string             `json:"token" bson:"token"`
+	UserID    string             `json:"userID" bson:"userID"`
+	Type      string             `json:"type" bson:"type"`
+	CreatedAt time.Time          `json:"createdAt" bson:"createdAt"`
+	UpdatedAt time.Time          `json:"updatedAt" bson:"updatedAt"`
+	ExpiresAt time.Time          `json:"expiresAt" bson:"UpdatedAt"`
+}
+
+type Organization struct {
+	ID    primitive.ObjectID `json:"_id" bson:"_id"`
+	Name  string             `json:"name" bson:"name"`
+	Users []UserOrganization `json:"users" bson:"users"`
+}
+
+type UserOrganization struct {
+	UserID  primitive.ObjectID `json:"_id" bson:"_id"`
+	IsAdmin bool               `json:"isAdmin" bson:"isAdmin"`
+}
